211a7913860631728e543ccc3b2bb1341f549e12
Snowspace <snxwspace@gmail.com>
2025-03-06 11:37:32 -0800

    made boundaries at edge of playable range; 2.1 finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,16 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 15.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -16,5 +18,11 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput *Time.deltaTime * speed);
+
+        // Keep player in bounds
+        if(math.abs(transform.position.x) > xRange) {
+            // the tutorial is bad and inefficient so i took it into my own hands
+            transform.position = new Vector3(xRange * math.sign(transform.position.x), transform.position.y, transform.position.z);
+        }
     }
 }
