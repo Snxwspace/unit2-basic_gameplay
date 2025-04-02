7de26fa86ee8d6098074d284b5157ac71909cda6
Snowspace <snxwspace@gmail.com>
2025-04-02 12:50:24 -0700

    added timed animal spawning

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,21 +6,27 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeX = 20;
     private float spawnPosZ = 29;
 
+    private float startDelay = 1.5f;
+    private float spawnInterval = 1.5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q)) {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        // if (Input.GetKeyDown(KeyCode.Q)) {
+        //     SpawnRandomAnimal();
+        // }
+    }
 
-            Instantiate(animalPrefabs[animalIndex], spawnPos, 
-                        animalPrefabs[animalIndex].transform.rotation);
-        }
+    void SpawnRandomAnimal() {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, 
+                    animalPrefabs[animalIndex].transform.rotation);
     }
 }
